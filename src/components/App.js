8acf2d3ec6ca9aaf63dbcf46f6fcf75276765379
shamin <shamin.asfaq@gmail.com>
2019-11-09 11:12:26 +0600

    Google Analytics integrated.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 import React, { lazy, Suspense } from 'react';
 import SearchBar from '../components/SearchBar';
-// import ImageList from './ImageList';
+import ReactGA from 'react-ga';
 import { connect } from 'react-redux';
 import { ImageListPlaceholder } from './placeholders/ImageListPlaceholder';
 
-// const LazyLoadComponentWrapper = lazy(() => import('./wrappers/LazyLoadComponentWrapper'));
 const ImageList = lazy(() => import('../components/ImageList'));
+
+const initializeAnalytics = () => {
+    ReactGA.initialize("UA-151912722-1");
+    ReactGA.pageview('/HomePage')
+}
 
 class App extends React.Component {
     render() {
+        initializeAnalytics()
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar />
